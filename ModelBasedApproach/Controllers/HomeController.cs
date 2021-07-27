b84e24a1fda8ec21246c456ee32545b1264b36af
yawar2019 <yawarali17@gmail.com>
2021-07-27 08:06:48 +0530

    committed

--- ModelBasedApproach/Controllers/HomeController.cs
+++ ModelBasedApproach/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using ModelBasedApproach.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -30,7 +31,34 @@ namespace ModelBasedApproach.Controllers
         {
             RegisterModel reg = new Models.RegisterModel();
             reg.UserName = "Jyothi";
+            EmployeeModelContainer db = new Models.EmployeeModelContainer();
+            ViewBag.Employees = new SelectList(db.Employeeinfoes, "EmpId", "EmpName",3);
             return View(reg);
+        }
+        [HttpPost]
+        public ActionResult HtmlHelperExample(string EmpName,int Employees)
+        {
+            
+            EmployeeModelContainer db = new Models.EmployeeModelContainer();
+            ViewBag.Employees = new SelectList(db.Employeeinfoes, "EmpId", "EmpName", 3);
+            var selected = db.Employeeinfoes.Where(s => s.EmpId == Employees).SingleOrDefault();
+            ViewBag.msg = EmpName+","+ selected.EmpName;
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index2(HttpPostedFileBase fileupload)
+        {
+            string filename = Path.GetFileName(fileupload.FileName);
+            string path = Server.MapPath("~/Upload");
+            fileupload.SaveAs(Path.Combine(path, filename));
+            ViewBag.msg = "uploaded successfully";
+
+            return View();
         }
 
     }
